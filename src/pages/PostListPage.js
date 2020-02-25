d6ce3fa114214db207a40ca7e912505cdec0d615
jisu <lee@jisuui-MacBookPro.local>
2020-02-26 02:53:38 +0900

    first Commit

---
+++ src/pages/PostListPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '../component/common/Button';
+const PostListPage = () => {
+  return (
+    <div>
+      <Button>버튼</Button>
+    </div>
+  );
+};
+
+export default PostListPage;
