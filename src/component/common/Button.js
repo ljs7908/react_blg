5e92332c48ed445936d3dc028a3ae610ddab5424
ljs7908 <dev_jisu@naver.com>
2020-03-13 00:34:36 +0900

    20200312

--- src/component/common/Button.js
+++ src/component/common/Button.js
@@ -7,7 +7,7 @@ const StyledButton = styled.button`
   border-radius: 4px;
   font-size: 1rem;
   font-weight: bold;
-  padding: 0.25rem 1rem;
+  padding: 0.25rem 0rem;
   color: white;
   outline: none;
   cursor: pointer;
